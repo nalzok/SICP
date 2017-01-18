807532e89d4689580861e93af1b054398e81f4ea
Sun Qingyao <sunqingyao19970825@gmail.com>
2017-01-18 23:16:54 +0800

    fix

--- chapter2/section2.1/ex2.16.scm
+++ chapter2/section2.1/ex2.16.scm
@@ -12,12 +12,17 @@
 ;;    
 ;;    Algebraically equivalent expressions are no longer equivalent when
 ;;    performing "interval arithmetic", in which the algebraic concept of
-;;    "equivalent" does not apply.
+;;    "equivalence" does not apply. In interval arithmetic, a equivalent
+;;    transformation can make things surprisingly different. For example,
+;;    as shown in ex2.14.scm, (div-interval r1 r1) isn't exactly 1±0, which
+;;    means multiplying the numerator and the denominator with a same value
+;;    would change the computed value of the fraction。
 
 ;; Q: Can you devise an interval-arithmetic package that does not have this
 ;;    shortcoming, or is this task impossible?
 ;;
 ;; A: I think it's possible. I can write a system which transforms all the
 ;;    expressions to their "best" form before computing them out using the
-;;    inexact interval arithmetic.
+;;    inexact interval arithmetic. However, writing such a package would
+;;    be very difficult.
 
